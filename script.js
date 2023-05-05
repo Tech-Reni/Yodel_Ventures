2e0b79f90dd6ca94971f851389c83d43dd1cd7e0
Tech-Reni <121473246+Tech-Reni@users.noreply.github.com>
2023-05-05 22:42:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,43 @@ add_customer.addEventListener('click', () => {
     add_customer.classList.toggle("active");
     cust_info.classList.toggle("active");
 })
+
+var addCust_info = document.getElementById("add_customer_info");
+var custName = document.getElementById("cust_name"); //THE CUSTOMER'S NAME
+var custAddress = document.getElementById("cust_address"); //THE CUSTOMER'S ADDRESS
+var custPhone = document.getElementById("cust_phoneNum"); //THE CUSTOMER'S PHONE NUMBER
+var custPurchase = document.getElementById("Num_of_eggs"); //HOW MANY EGGS THE CUSTOMER BOUGHT
+
+var form = document.querySelector("form");
+
+let Sales = [];
+
+const addSale = () => {
+    let sale = {
+        customersName: custName.value,
+        customersAddress: custAddress.value,
+        customersPhone: custPhone.value,
+        customersPurchase: custPurchase.value
+    }
+
+    Sales.push(sale)
+
+    // To clear the inputs
+    custName.value = "";
+    custAddress.value = "";
+    custPhone.value = "";
+    custPurchase.value = "";
+    //Clearing the input ends here
+
+    //Saving to LocalStorage
+    localStorage.setItem('Sales', JSON.stringify(sale));
+
+    console.log(Sales);
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    addCust_info.addEventListener('click', () => {
+        addSale();
+    })
+})
